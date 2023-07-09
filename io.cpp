6b02d8fb37f08a762e875038c6f217410babcdcc
shashwat singh <shashwatsingh@shashwats-MacBook-Air.local>
2023-07-09 07:00:17 +0530

    the first day for cpp

---
+++ io.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num1,num2;
+    cout<<"enter first number: ";
+    cin>>num1;
+    cout<<"enter second number: ";
+    cin>>num2;
+    cout<<"the sum of two number is "<<num1+num2;
+    return 0;
+}
