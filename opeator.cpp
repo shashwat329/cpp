858233072904e9b052044e284169679e08f7ee43
shashwat329 <shashwatrajwanshi41@gmail.com>
2023-07-10 23:28:38 +0530

    day 02 of learning cpp

---
+++ opeator.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n1,n2;
+    char c;
+    cout<<"enter the first number: ";
+    cin>>n1;
+    cout<<"enter the second number: ";
+    cin>>n2;
+    cout<<"The sum of entered number is "<<n1+n2<<endl;
+    cout<<"The diff of entered number is "<<n1-n2<<endl;
+    cout<<"The multipication of entered number is "<<n1*n2<<endl;
+    cout<<"The divison of entered number is "<<n1/n2<<endl;
+    return 0;
+}
