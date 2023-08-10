18f7fc27cee40b6c8ebb360ccbe05193458462a9
shashwat329 <shashwatrajwanshi41@gmail.com>
2023-08-10 23:33:24 +0530

    day 10

--- OOPs/objectsarg.cpp
+++ OOPs/objectsarg.cpp
@@ -5,7 +5,27 @@ using namespace std;
 class complex{
     int a,b;
     public:
-    void setcomplex(){
-        
+    void setcomplex(int x,int y){
+        a= x;
+        b = y;
+    }
+    void getcomplex(void){
+        cout<<"the complex number is "<<a<<" + "<<b<<"i"<<endl;
+    }
+    int sumcomplex(complex o1,complex o2){
+        a =o1.a+o2.a;
+        b =o1.b+o2.b;
+        return (a+b);
     }
 };
+int main(){
+    complex c1,c2,c3;
+    c1.setcomplex(3,4);
+    c1.getcomplex();
+    c2.setcomplex(2,3);
+    c2.getcomplex();
+    c3.sumcomplex(c1,c2);
+    c3.getcomplex();
+    return 0;
+
+}
